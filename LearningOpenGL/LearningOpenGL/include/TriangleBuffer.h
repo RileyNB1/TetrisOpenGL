12acfe9034637c9c652a9cf54cf475473fadaf32
jajanpreetsingh <jajanpreetsingh15@gmail.com>
2023-08-16 19:29:24 -0400

    Model view tutorial

--- LearningOpenGL/LearningOpenGL/include/TriangleBuffer.h
+++ LearningOpenGL/LearningOpenGL/include/TriangleBuffer.h
@@ -1,29 +1,10 @@
 #pragma once
 
-#include <GL_Lib.h>
+#include <Macros.h>
 
 #include <vector>
 #include <Window.h>
 #include <Shader.h>
-
-//macro to write shader programs inline
-#ifndef GLSL 
-#define GLSL(version,A) "#version " #version "\n" #A
-#endif
-
-//macro to pick the right glGenVertexArray function
-#ifndef GENVERTEXARRAYS
-#define GENVERTEXARRAYS(n,id) \
-  if(GLEW_APPLE_vertex_array_object) glGenVertexArraysAPPLE(1,id);\
-	else if (GLEW_ARB_vertex_array_object) glGenVertexArrays(n,id)
-#endif
-
-//macro to pick the right glBindVertexArray function
-#ifndef BINDVERTEXARRAY
-#define BINDVERTEXARRAY(id) \
-  if(GLEW_APPLE_vertex_array_object) glBindVertexArrayAPPLE(id);\
-	else if (GLEW_ARB_vertex_array_object) glBindVertexArray(id)
-#endif
 
 /*-----------------------------------------------------------------------------
  *  A PLAIN-OLD-DATA ("POD") Container for 2D Coordinates (eventually we'll use a
@@ -52,24 +33,25 @@ public:
 class Vertex
 {
 public:
-    Vec2 position;
-    Vec4 color;
+    glm::vec2 position;
+    glm::vec4 color;
 };
 
 namespace FOGrP
 {
     class TriangleBuffer
     {
-        std::vector<Vertex> mTris;
+        Vertex mTris[3];
         Shader *mTrisShader;
 
         //ID of Vertex Attribute
         GLuint positionID, colorID, orthoID;
+
+        GLuint modelID, viewID, projectionID;
         //A buffer ID
         GLuint bufferID;
         //A Vertex Array ID
         GLuint arrayID;
-
     public:
 
         TriangleBuffer();
