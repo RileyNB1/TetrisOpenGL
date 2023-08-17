b19ab145090f540e870740c49503746098c0a26a
jajanpreetsingh <jajanpreetsingh15@gmail.com>
2023-08-16 20:22:53 -0400

    Revert "Model view tutorial"
    This reverts commit 12acfe9034637c9c652a9cf54cf475473fadaf32.

--- LearningOpenGL/LearningOpenGL/include/TriangleBuffer.h
+++ LearningOpenGL/LearningOpenGL/include/TriangleBuffer.h
@@ -1,10 +1,29 @@
 #pragma once
 
-#include <Macros.h>
+#include <GL_Lib.h>
 
 #include <vector>
 #include <Window.h>
 #include <Shader.h>
+
+//macro to write shader programs inline
+#ifndef GLSL 
+#define GLSL(version,A) "#version " #version "\n" #A
+#endif
+
+//macro to pick the right glGenVertexArray function
+#ifndef GENVERTEXARRAYS
+#define GENVERTEXARRAYS(n,id) \
+  if(GLEW_APPLE_vertex_array_object) glGenVertexArraysAPPLE(1,id);\
+	else if (GLEW_ARB_vertex_array_object) glGenVertexArrays(n,id)
+#endif
+
+//macro to pick the right glBindVertexArray function
+#ifndef BINDVERTEXARRAY
+#define BINDVERTEXARRAY(id) \
+  if(GLEW_APPLE_vertex_array_object) glBindVertexArrayAPPLE(id);\
+	else if (GLEW_ARB_vertex_array_object) glBindVertexArray(id)
+#endif
 
 /*-----------------------------------------------------------------------------
  *  A PLAIN-OLD-DATA ("POD") Container for 2D Coordinates (eventually we'll use a
@@ -33,25 +52,24 @@ public:
 class Vertex
 {
 public:
-    glm::vec2 position;
-    glm::vec4 color;
+    Vec2 position;
+    Vec4 color;
 };
 
 namespace FOGrP
 {
     class TriangleBuffer
     {
-        Vertex mTris[3];
+        std::vector<Vertex> mTris;
         Shader *mTrisShader;
 
         //ID of Vertex Attribute
         GLuint positionID, colorID, orthoID;
-
-        GLuint modelID, viewID, projectionID;
         //A buffer ID
         GLuint bufferID;
         //A Vertex Array ID
         GLuint arrayID;
+
     public:
 
         TriangleBuffer();
--- LearningOpenGL/LearningOpenGL/src/TriangleBuffer.cpp
+++ LearningOpenGL/LearningOpenGL/src/TriangleBuffer.cpp
@@ -2,36 +2,35 @@
 
 namespace FOGrP
 {
+
     /*-----------------------------------------------------------------------------
      *  SOME SHADER CODE
      *-----------------------------------------------------------------------------*/
     const char* vert = GLSL(120,
 
-        attribute vec4 position;
+    attribute vec4 position;
     attribute vec4 color;
 
     varying vec4 dstColor;
 
-    uniform mat4 model;
-    uniform mat4 view;                 //<-- 4x4 Transformation Matrices
-    uniform mat4 projection;
+    uniform mat4 ortho;                //<-- new uniform matrix
 
     void main() {
         dstColor = color;
-        gl_Position = projection * view * model * position;   //<-- Apply transformation 
+        gl_Position = ortho * position;  //<-- matrix gets multiplied by the position
     }
 
     );
 
     const char* frag = GLSL(120,
-        varying vec4 dstColor;
+
+    varying vec4 dstColor;
 
     void main() {
         gl_FragColor = dstColor;
     }
 
     );
-
 
     TriangleBuffer::TriangleBuffer()
     {    
@@ -41,19 +40,19 @@ namespace FOGrP
     {
         Vertex v1, v2, v3;
 
-        v1.position = glm::vec2(-1, 0);
-        v1.color = glm::vec4(1, 0, 0, 1);
+        v1.position = Vec2(-1, -0.5);
+        v1.color = Vec4(1, 0, 0, 1);
 
-        v2.position = glm::vec2(0, 1);
-        v2.color = glm::vec4(0, 1, 0, 1);
+        v2.position = Vec2(0, 1);
+        v2.color = Vec4(0, 1, 0, 1);
 
-        v3.position = glm::vec2(1, 0);
-        v3.color = glm::vec4(0, 0, 1, 1);
+        v3.position = Vec2(1, -0.5);
+        v3.color = Vec4(0, 1, 0, 1);
 
         //Specify the 3 VERTICES of A Triangle
-        mTris[0] = v1;
-        mTris[1] = v2;
-        mTris[2] = v3;
+        mTris.push_back(v1);
+        mTris.push_back(v2);
+        mTris.push_back(v3);
 
         /*-----------------------------------------------------------------------------
          *  CREATE THE SHADER
@@ -65,21 +64,19 @@ namespace FOGrP
 
     void TriangleBuffer::BindVertexData()
     {
-        // Get attribute locations
         positionID = glGetAttribLocation(mTrisShader->Id(), "position");
         colorID = glGetAttribLocation(mTrisShader->Id(), "color");
+        orthoID = glGetUniformLocation(mTrisShader->Id(), "ortho");
 
-        // Get uniform locations
-        modelID = glGetUniformLocation(mTrisShader->Id(), "model");
-        viewID = glGetUniformLocation(mTrisShader->Id(), "view");
-        projectionID = glGetUniformLocation(mTrisShader->Id(), "projection");
+        glUseProgram(0);
 
-        /*-----------------------------------------------------------------------------
-         *  CREATE THE VERTEX ARRAY OBJECT
-         *-----------------------------------------------------------------------------*/
+
+        //| x | y | r | g | b | a| |x | y | r | g | b | a| |x | y | r | g | b | a |
+            /*-----------------------------------------------------------------------------
+             *  CREATE THE VERTEX ARRAY OBJECT
+             *-----------------------------------------------------------------------------*/
         GENVERTEXARRAYS(1, &arrayID);
         BINDVERTEXARRAY(arrayID);
-
 
         /*-----------------------------------------------------------------------------
          *  CREATE THE VERTEX BUFFER OBJECT
@@ -89,7 +86,7 @@ namespace FOGrP
         // Bind Array Buffer 
         glBindBuffer(GL_ARRAY_BUFFER, bufferID);
         // Send data over buffer to GPU
-        glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vertex), &(mTris[0]), GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, mTris.size() * sizeof(Vertex), &(mTris[0]), GL_STATIC_DRAW);
 
 
         /*-----------------------------------------------------------------------------
@@ -102,64 +99,32 @@ namespace FOGrP
 
         // Tell OpenGL how to handle the buffer of data that is already on the GPU
         glVertexAttribPointer(positionID, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-        glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)sizeof(glm::vec2));
+        glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)sizeof(Vec2));
 
-        //now we can unbind vertex array object and vertex buffer object
         BINDVERTEXARRAY(0);
+
+        /// <summary>
+        /// Not in colored tris buffer
+        /// </summary>
         glBindBuffer(GL_ARRAY_BUFFER, 0);
     }
 
     void TriangleBuffer::Draw(const Window& window)
     {
-        static float time = 0.0;
-        time += .01;
-
         //Bind Shader and Vertex Array Object
         glUseProgram(mTrisShader->Id());
+
+        //glm is picky about using floats: use .f!
+        glm::mat4 ortho = glm::ortho(-window.AspectRatio(), window.AspectRatio(), -1.f, 1.f, -1.f, 1.f);
+        //set uniform in shader
+        glUniformMatrix4fv(orthoID, 1, GL_FALSE, glm::value_ptr(ortho));
+
         BINDVERTEXARRAY(arrayID);
 
-        /*-----------------------------------------------------------------------------
-         *  Set up view and projection matrices
-         *-----------------------------------------------------------------------------*/
-        glm::vec3 eyepos = glm::vec3(cos(time) * 1.0, 0, sin(time) * 5.0);
+        //Draw Triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glm::mat4 view = glm::lookAt(eyepos,                           //eye
-            glm::vec3(0, 0, 0),                 //target
-            glm::vec3(0, 1, 0));               //up
-
-        glm::mat4 proj = glm::perspective(PI / 3.0f, (float)window.AspectRatio(), 0.1f, -10.0f);
-
-        glUniformMatrix4fv(viewID, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(projectionID, 1, GL_FALSE, glm::value_ptr(proj));
-
-        /*-----------------------------------------------------------------------------
-         *  Render a whole bunch of spinning triangles
-         *-----------------------------------------------------------------------------*/
-        int len = 10; int total = len * len * len;
-        for (int i = 0; i < len; ++i) {
-            for (int j = 0; j < len; ++j) {
-                for (int k = 0; k < len; ++k) {
-                    int idx = i * len * len + j * len + k;
-
-                    glm::vec3 trs(-len / 2.0 + i, -len / 2.0 + j, -len / 2.0 + k);
-                    glm::vec3 axis(0, 0, 1);
-                    float rad = time * PI;
-                    glm::vec3 vscale(1.0 - (float)idx / total);
-
-                    glm::mat4 translation = glm::translate(glm::mat4(), trs);
-                    glm::mat4 rotation = glm::rotate(glm::mat4(), rad, axis);
-                    glm::mat4 scale = glm::scale(glm::mat4(), vscale);
-
-                    //ORDER MATTERS!
-                    glm::mat4 model = translation * rotation * scale;
-
-                    glUniformMatrix4fv(modelID, 1, GL_FALSE, glm::value_ptr(model));
-                    glDrawArrays(GL_TRIANGLES, 0, 3);
-                }
-            }
-        }
-
-        //Unbind 
+        //Unbind Vertex Array Object and Shader
         BINDVERTEXARRAY(0);
         glUseProgram(0);
     }
