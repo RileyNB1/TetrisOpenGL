784a4e4df281f470eceed45d662d3f5b56ad91ce
jajanpreetsingh <jajanpreetsingh15@gmail.com>
2023-08-16 20:54:50 -0400

    remove unused code

--- LearningOpenGL/LearningOpenGL/LearningOpenGL.cpp
+++ LearningOpenGL/LearningOpenGL/LearningOpenGL.cpp
@@ -5,25 +5,6 @@
 #define GLSL(version,A) "#version " #version "\n" #A
 #endif // ! GLSL
 
-void Draw()
-{
-    glBegin(GL_TRIANGLES);
-
-    //Vertex 1
-    glColor3f(1, 0, 0); 
-    glVertex3f(-1, 0, 0);
-
-    //Vertex 2
-    glColor3f(0, 1, 0);
-    glVertex3f(0, 1, 0);
-
-    //Vertex 3
-    glColor3f(0, 0, 1);
-    glVertex3f(1, 0, 0);
-
-    glEnd();
-}
-
 
 int main()
 {
